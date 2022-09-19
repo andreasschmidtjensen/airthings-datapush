978de01e9ef617b79c8eab227e92c4ff092688fb
Andreas Schmidt Jensen <andreas.s.jensen@gmail.com>
2022-09-19 23:46:27 +0200

    store only value, not unit

--- influx_writer.py
+++ influx_writer.py
@@ -12,7 +12,7 @@ def write_to_influxdb(data: Dict[str, float]):
     with InfluxDBClient(url=os.environ["INFLUX_URL"], token=token, org=org) as client:
         write_api = client.write_api(write_options=SYNCHRONOUS)
 
-        sequence = [f"airquality,host={os.uname()[1]} {k}={v}" for k, v in data.items()]
+        sequence = [f"airquality,host={os.uname()[1]} {k}={v['value']}" for k, v in data.items()]
         write_api.write(bucket, org, sequence)
 
     client.close()
